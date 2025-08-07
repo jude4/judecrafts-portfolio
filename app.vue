94b5c84ca561a5c5c5ad1d9b95cfb8bcaaded396
Jude <judeufuomao@gmail.com>
2025-08-07 09:43:39 +0100

    feat: added animation and changed the image asset image

--- app.vue
+++ app.vue
@@ -4,17 +4,14 @@
       <!-- <NuxtRouteAnnouncer /> -->
       <!-- into section -->
       <div
-        class="relative h-[400px] bg-gradient-to-t from-indigo-200 lg:h-screen dark:from-slate-800 dark:to-slate-900"
-      >
+        class="relative h-[400px] bg-gradient-to-t from-indigo-200 lg:h-screen dark:from-slate-800 dark:to-slate-900">
         <nav class="fixed top-0 w-full px-5 bg-white z-10 dark:bg-slate-900">
           <div class="container flex justify-between items-center py-5 mx-auto">
             <div class="flex gap-2 items-center">
-               <Logo :isDark="isDark" />
+              <Logo :isDark="isDark" />
             </div>
 
-            <ul
-              class="hidden space-x-10 text-sm font-bold text-gray-600 dark:text-gray-100 uppercase md:flex"
-            >
+            <ul class="hidden space-x-10 text-sm font-bold text-gray-600 dark:text-gray-100 uppercase md:flex">
               <li class="hover:text-gray-500">
                 <a href="#">Home</a>
               </li>
@@ -37,48 +34,33 @@
             </ul>
             <div class="flex items-center gap-2">
 
-              <img
-                src="./assets/img/moon.png"
-                class=" w-5 cursor-pointer md:block"
-                alt=""
-                @click="isDark = !isDark"
-              />
-              <div
-                v-if="!menu"
-                class="z-20 space-y-1 cursor-pointer md:hidden"
-                @click="toggleMenu"
-              >
+              <img src="./assets/img/moon.png" class=" w-5 cursor-pointer md:block" alt="" @click="isDark = !isDark" />
+              <div v-if="!menu" class="z-20 space-y-1 cursor-pointer md:hidden" @click="toggleMenu">
                 <div class="w-6 h-0.5 bg-black dark:bg-gray-100"></div>
                 <div class="w-6 h-0.5 bg-black dark:bg-gray-100"></div>
                 <div class="w-6 h-0.5 bg-black dark:bg-gray-100"></div>
               </div>
               <div class="z-20" v-else>
-                <span
-                  class="w-6 h-0.5 text-2xl text-white cursor-pointer md:hidden"
-                  @click="toggleMenu"
-                  >X</span
-                >
+                <span class="w-6 h-0.5 text-2xl text-white cursor-pointer md:hidden" @click="toggleMenu">X</span>
               </div>
-              <ul
-                :class="menu ? 'block' : 'hidden'"
-                class="absolute top-0 left-0 p-10 space-y-10 w-full text-center text-white bg-indigo-900 rounded-b-3xl"
-              >
+              <ul :class="menu ? 'block' : 'hidden'"
+                class="absolute top-0 left-0 p-10 space-y-10 w-full text-center text-white bg-indigo-900 rounded-b-3xl">
                 <li>
                   <a href="#" @click="toggleMenu">Home</a>
                 </li>
-  
+
                 <li>
                   <a href="#about" @click="toggleMenu">About me</a>
                 </li>
-  
+
                 <li>
                   <a href="#services" @click="toggleMenu">Services</a>
                 </li>
-  
+
                 <li>
                   <a href="#works" @click="toggleMenu">Works</a>
                 </li>
-  
+
                 <li>
                   <a href="#contact" @click="toggleMenu">Contact</a>
                 </li>
@@ -89,60 +71,48 @@
 
         <!-- Intro content -->
         <!-- image -->
-        <img
-          src="./assets/img/man.png"
-          class="object-cover absolute right-0 bottom-0 mx-auto h-5/6 lg:left-0"
-          alt="hero back image"
-        />
+        <img src="./assets/img/avatar.jpg"
+          class="object-contain absolute right-0 bottom-0 rounded-full mx-auto  h-5/6 lg:left-0"
+          alt="hero back image" />
         <!-- circle -->
         <div
-          class="hidden lg:block absolute bottom-0 w-big -z-10 h-big bg-indigo-900 rounded-full right-0 left-0 mx-auto"
-        ></div>
+          class="hidden lg:block absolute bottom-0 w-big -z-10 h-big bg-indigo-900 rounded-full right-0 left-0 mx-auto">
+        </div>
         <!--animated text  -->
         <div
-          class="absolute top-1/3 left-5 text-xl sm:left-10 sm:text-4xl md:left-1/4 md:text-6xl lg:left-5 xl:left--48 xl:text-7xl font-bold"
-        >
+          class="absolute top-1/3 left-5 text-xl sm:left-10 sm:text-4xl md:left-1/4 md:text-6xl lg:left-5 xl:left--48 xl:text-7xl font-bold">
           <span class="text-gray-600">Freelance</span>
-          <AutoTyperVue
-            id="main-auto-typer"
-            :typingDelay="200"
-            componentTag="h1"
-            :text="text"
-          />
+          <AutoTyperVue id="main-auto-typer" :typingDelay="200" componentTag="h1" :text="text" />
         </div>
         <!-- text -->
         <div
-          class="hidden lg:flex flex-col absolute top-0 bottom-0 m-auto shadow-lg right-10 gap-5 rounded-sm bg-white p-6 h-fit w-1/3 dark:shadow-slate-800 dark:bg-slate-900"
-        >
+          class="hidden lg:flex flex-col absolute top-0 bottom-0 m-auto shadow-lg right-10 gap-5 rounded-sm bg-white p-6 h-fit w-1/3 dark:shadow-slate-800 dark:bg-slate-900 animate-[bounce_2s_ease-in-out_infinite]">
           <h1 class="text-4xl font-semibold text-indigo-900">Hi, i'm Jude</h1>
           <p class="text-gray-400">
-            With over 6 years of experience on web design and development. Lorem
+            With over 8 years of experience on web design and development. Lorem
             ipsum dolor sit amet consectetur adipisicing elit. N oumquam quo
             provident, facere minus temporibus veniam nostrum reprehenderit
             nihil?
           </p>
-          <a
-            class="bg-indigo-600 hover:bg-indigo-500 py-2 px-3 text-xl w-fit font-semibold rounded-md text-white"
-            href="#contact"
-            >Hire Me</a
-          >
+          <a class="bg-indigo-600 hover:bg-indigo-500 py-2 px-3 text-xl w-fit font-semibold rounded-md text-white"
+            href="https://linkedin.com/in/jude-onohwosafe/" target="_blank">Contact Me</a>
         </div>
       </div>
 
       <!-- about -->
       <div class="px-10 dark:bg-slate-900" id="about">
-        <div
-          class="container mx-auto flex flex-col-reverse lg:flex-row py-40 items-center gap-20"
-        >
+        <div class="container mx-auto flex flex-col-reverse lg:flex-row py-40 items-center gap-20">
           <!-- left size -->
           <div class="relative">
-            <img
-              src="./assets/img/dots.png"
-              class="h-1/4 -z-10 absolute top-0 left-0 w-1/4"
-              alt=""
-            />
+            <img src="./assets/img/dots.png" class="h-1/4 -z-10 absolute top-0 left-0 w-1/4" alt="" />
             <div class="h-full rounded-full overflow-hidden">
-              <img src="./assets/img/portrait.png" alt="" />
+              <svg width="100" height="200" xmlns="http://www.w3.org/2000/svg">
+                <rect width="100" height="200" fill="#4B0082" opacity="0.8" />
+                <circle cx="50" cy="50" r="30" fill="#9370DB" />
+                <line x1="20" y1="100" x2="80" y2="150" stroke="#DDA0DD" stroke-width="4" />
+                <polygon points="40,180 60,180 50,160" fill="#DDA0DD" />
+              </svg>
+              <!-- <img src="./assets/img/portrait.png" alt="" /> -->
             </div>
           </div>
           <!-- right size -->
@@ -205,9 +175,7 @@
         <!-- bottom -->
         <div class="flex flex-wrap p-5 sm:p-0">
           <!-- card -->
-          <div
-            class="w-full md:w-4/12 shadow-xl rounded-lg p-5 my-3 md:my-10 flex flex-col gap-3"
-          >
+          <div class="w-full md:w-4/12 shadow-xl rounded-lg p-5 my-3 md:my-10 flex flex-col gap-3">
             <img src="./assets/img/icon.png" class="w-10" alt="" />
             <h1 class="font-medium text-lg dark:text-white">UX / UI Design</h1>
             <p class="text-gray-400">
@@ -218,9 +186,7 @@
           </div>
 
           <!-- card -->
-          <div
-            class="w-full md:w-4/12 shadow-xl rounded-lg p-5 my-3 md:my-10 flex flex-col gap-3"
-          >
+          <div class="w-full md:w-4/12 shadow-xl rounded-lg p-5 my-3 md:my-10 flex flex-col gap-3">
             <img src="./assets/img/icon.png" class="w-10" alt="" />
             <h1 class="font-medium text-lg dark:text-white">
               Web Applications
@@ -234,9 +200,7 @@
           </div>
 
           <!-- card -->
-          <div
-            class="w-full md:w-4/12 shadow-xl rounded-lg p-5 my-3 md:my-10 flex flex-col gap-3"
-          >
+          <div class="w-full md:w-4/12 shadow-xl rounded-lg p-5 my-3 md:my-10 flex flex-col gap-3">
             <img src="./assets/img/icon.png" class="w-10" alt="" />
             <h1 class="font-medium text-lg dark:text-white">
               Custom CRM & Admin Dashboards
@@ -248,9 +212,7 @@
           </div>
 
           <!-- card -->
-          <div
-            class="w-full md:w-4/12 shadow-xl rounded-lg p-5 my-3 md:my-10 flex flex-col gap-3"
-          >
+          <div class="w-full md:w-4/12 shadow-xl rounded-lg p-5 my-3 md:my-10 flex flex-col gap-3">
             <img src="./assets/img/icon.png" class="w-10" alt="" />
             <h1 class="font-medium text-lg dark:text-white">
               DevOps & Deployments
@@ -264,9 +226,7 @@
           </div>
 
           <!-- card -->
-          <div
-            class="w-full md:w-4/12 shadow-xl rounded-lg p-5 my-3 md:my-10 flex flex-col gap-3"
-          >
+          <div class="w-full md:w-4/12 shadow-xl rounded-lg p-5 my-3 md:my-10 flex flex-col gap-3">
             <img src="./assets/img/icon.png" class="w-10" alt="" />
             <h1 class="font-medium text-lg dark:text-white">
               Maintenance & Support
@@ -280,9 +240,7 @@
           </div>
 
           <!-- card -->
-          <div
-            class="w-full md:w-4/12 shadow-xl rounded-lg p-5 my-3 md:my-10 flex flex-col gap-3"
-          >
+          <div class="w-full md:w-4/12 shadow-xl rounded-lg p-5 my-3 md:my-10 flex flex-col gap-3">
             <img src="./assets/img/icon.png" class="w-10" alt="" />
             <h1 class="font-medium text-lg dark:text-white">
               Consulting, Mentoring, and Code Review
@@ -314,30 +272,22 @@
         <!-- bottom -->
         <div class="flex flex-wrap p-5 sm:p-0 justify-between">
           <!-- card -->
-          <div
-            class="w-full md:w-5/12 lg:w-1/5 shadow-xl rounded-lg my-3 md:my-110 m-1"
-          >
+          <div class="w-full md:w-5/12 lg:w-1/5 shadow-xl rounded-lg my-3 md:my-110 m-1">
             <img src="./assets/img/item.png" alt="" />
           </div>
 
           <!-- card -->
-          <div
-            class="w-full md:w-5/12 lg:w-1/5 shadow-xl rounded-lg my-3 md:my-110 m-1"
-          >
+          <div class="w-full md:w-5/12 lg:w-1/5 shadow-xl rounded-lg my-3 md:my-110 m-1">
             <img src="./assets/img/item.png" alt="" />
           </div>
 
           <!-- card -->
-          <div
-            class="w-full md:w-5/12 lg:w-1/5 shadow-xl rounded-lg my-3 md:my-110 m-1"
-          >
+          <div class="w-full md:w-5/12 lg:w-1/5 shadow-xl rounded-lg my-3 md:my-110 m-1">
             <img src="./assets/img/item.png" alt="" />
           </div>
 
           <!-- card -->
-          <div
-            class="w-full md:w-5/12 lg:w-1/5 shadow-xl rounded-lg my-3 md:my-110 m-1"
-          >
+          <div class="w-full md:w-5/12 lg:w-1/5 shadow-xl rounded-lg my-3 md:my-110 m-1">
             <img src="./assets/img/item.png" alt="" />
           </div>
         </div>
@@ -360,24 +310,14 @@
         <form action="" class="flex p-8 flex-col gap-5 items-center mt-5">
           <input
             class="p-2 w-full md:w-1/2 ring-1 ring-indigo-300 rounded-sm dark:bg-slate-800 dark:text-white dark:ring-0"
-            type="text"
-            placeholder="Name"
-          />
+            type="text" placeholder="Name" />
           <input
             class="p-2 w-full md:w-1/2 ring-1 ring-indigo-300 rounded-sm dark:bg-slate-800 dark:text-white dark:ring-0"
-            type="email"
-            placeholder="Email"
-          />
-          <textarea
-            name=""
-            rows="10"
+            type="email" placeholder="Email" />
+          <textarea name="" rows="10"
             class="p-2 w-full md:w-1/2 ring-1 ring-indigo-300 rounded-sm dark:bg-slate-800 dark:text-white dark:ring-0"
-            id=""
-            placeholder="Type here..."
-          ></textarea>
-          <button
-            class="w-1/2 bg-indigo-600 text-white font-medium px-3 py-2 rounded-md cursor-pointer"
-          >
+            id="" placeholder="Type here..."></textarea>
+          <button class="w-1/2 bg-indigo-600 text-white font-medium px-3 py-2 rounded-md cursor-pointer">
             Submit
           </button>
         </form>
@@ -391,31 +331,13 @@
           <Logo :isDark="true" />
         </div>
 
-        <span class="text-white hidden md:block font-medium"
-          >© 2025 JudeCrafts. Design with ♥ by Jude</span
-        >
+        <span class="text-white hidden md:block font-medium">© 2025 JudeCrafts. Design with ♥ by Jude</span>
 
         <div class="flex gap-2">
-          <img
-            src="./assets/img/facebook.png"
-            class="w-4 cursor-pointer"
-            alt=""
-          />
-          <img
-            src="./assets/img/instagram.png"
-            class="w-4 cursor-pointer"
-            alt=""
-          />
-          <img
-            src="./assets/img/twitter.png"
-            class="w-4 cursor-pointer"
-            alt=""
-          />
-          <img
-            src="./assets/img/linkedin.png"
-            class="w-4 cursor-pointer"
-            alt=""
-          />
+          <img src="./assets/img/facebook.png" class="w-4 cursor-pointer" alt="" />
+          <img src="./assets/img/instagram.png" class="w-4 cursor-pointer" alt="" />
+          <img src="./assets/img/twitter.png" class="w-4 cursor-pointer" alt="" />
+          <img src="./assets/img/linkedin.png" class="w-4 cursor-pointer" alt="" />
         </div>
       </div>
     </div>
@@ -437,7 +359,20 @@ const isDark = ref(false);
 </script>
 <style scoped>
 @import "auto-typer-vue3/dist/style.css";
+
 #main-auto-typer {
   color: red;
+}
+
+@keyframes bounce {
+
+  0%,
+  100% {
+    transform: translateY(0);
+  }
+
+  50% {
+    transform: translateY(-10px);
+  }
 }
 </style>
